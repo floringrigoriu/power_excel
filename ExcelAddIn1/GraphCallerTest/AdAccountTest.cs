ad80f622811274f7869015cffe0ff749bb8129eb
floringrigoriu <florin_grigoriu@yahoo.com>
2015-01-19 17:13:06 -0800

    add test on perfomance - FB client is the slow dude

--- ExcelAddIn1/GraphCallerTest/AdAccountTest.cs
+++ ExcelAddIn1/GraphCallerTest/AdAccountTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GraphCaller;
+using System.Diagnostics;
 
 namespace GraphCallerTest
 {
@@ -8,9 +9,16 @@ namespace GraphCallerTest
     public class AdAccountTest
     {
         // This is an old invalidate valude , but should serve as an warning no leakage of sensitive and sensible data
-        public static readonly string testAdAccount = "CAACZBwbJIzL0BAPXZC7kufhmUsbBgaB8A8ZCh9SEQx6Ms4BpNCo1xPlvdJHjG263qCJRNK7Moa15zpPQCpdxMrxOHCb4aZB5zDSQBLpjsUWKfZCBZCH80cpZBUDkZCQlqoZAT7v4TFKAOQbFPnKWezMe3tMQnhYZCTQePLL7FJVRexAmkxSJFtU8kO";
+        public static readonly string testAdAccount = "CAACZBwbJIzL0BAEnYg5GBIGUO4d8j4TZC9ZCb7dDd8VfosR6FpzTLjXJBZBfTTrbEeIvaZB9jS8TzDMgDwGkxKrG3P4wEGydCaWDyaN3JsYt7TLtExPWo0mN5B1YEd4nkGDB1LWJDWV1msBGfFXjOQzRNPxUmPZCYerK9LNijabl9MyZA6IngAw96SheytMyNXAGlgrME4BZCwZDZD";
             //do update with the real deal - access token
-        public const long florinAdAccount = 10151318637546538;  
+        public const long florinAdAccount = 10151318637546538;
+
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
 
        [TestMethod]
         public void GetAdAccount()
@@ -29,6 +37,26 @@ namespace GraphCallerTest
             var adgroup = AdGroups.getAdGroup(testAdAccount, florinAdAccount);
             // assert
             //Assert.AreEqual(florinAdAccount, adgroup.AdgroupAccontID);
+        }
+
+        [TestMethod]
+        public void GetAdAccountNOWCF()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            // call
+            var account = AdAccount.getAdAccount(testAdAccount, florinAdAccount);
+            
+            // test result
+            Assert.AreEqual(florinAdAccount, account.AccontID);
+            Assert.IsTrue(account.DailySpend > 0);
+
+            // test time
+            stopwatch.Stop();
+            Assert.IsTrue(stopwatch.ElapsedMilliseconds < 1000,
+                "Expect fast exececution, faster than 1s, instead {0} ms",
+                stopwatch.ElapsedMilliseconds);
+            this.TestContext.WriteLine("test completed in {0} ms.", stopwatch.ElapsedMilliseconds);
         }
         
     }
